2b4b2dbd657331b129b0a0a19932e3ec792b50e1
Mig8at <mig8at@gmail.com>
2025-01-31 18:22:32 -0500

    Update API Gateway stage name to use environment variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -260,7 +260,7 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 resource "aws_api_gateway_stage" "dev" {
-  stage_name    = "dev"
+  stage_name    = var.environment
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.deployment.id
 }
